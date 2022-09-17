179301a8a794a06326d3697928b70e9afb944f61
iPappyTech <pappythorium@gmail.com>
2022-09-17 22:52:38 +0100

    updated

--- stopwatch.js
+++ stopwatch.js
@@ -48,14 +48,25 @@ const search =()=>{
         <h2 style="padding-top: 30px; text-align: center;">Activity Dashboard</h2>
     </div>
     <div style="background-color: white; height: 55vh;">
-       <div style="margin: auto; width: 80%; height: 40vh; background-color: #a79eda; padding-top: 60px;">
-            <input type="text" placeholder="active activty atm?" style="text-align: center;">
-            <input type="text" placeholder="duration used(min/secs/mm)" style="text-align: center;">
-            <input type="text" placeholder="more info- optional" style="text-align: center;">
+       <div style=" height: 45vh; background-color: #a79eda; padding-top: 20px;">
+            <div style="margin: auto; width: 70%; display: grid; gap: 5px;">
+                <label for="">DAY</label>
+                <input type="text" id="activity" placeholder="day of activity(1-30)" style="text-align: center;">
+                <div style="margin-top: 20px; display: grid; justify-content: center;">
+                    <h2>loremjfne</h2>
+                    <p>loremjfne</p>
+                    <p>loremjfne</p>
+                </div>
+            </div>
        </div>
-       <div style="display: flex; justify-content: center; margin-top: 20px;">
-            <button>SAVED</button>
-            <button>SEARCH</button>
+       <div style="display: flex; justify-content: center; margin-top: 20px; gap:20px;">
+            <button onclick="search()">SEARCH</button>
+            <button onclick="search()" style="background-color: #9559cb; border: none;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                    <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+                /svg>
+            </button>
        </div>
     </div>
 </section>`
